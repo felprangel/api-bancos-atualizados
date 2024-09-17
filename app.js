1f7c7524c2830acd5d26dc072ce632d81004233c
felprangel <feliperangel897@gmail.com>
2024-09-16 22:32:49 -0300

    adiciona código para rota de status

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/status", (req, res) => {});
+app.get("/status", (req, res) => {
+  const filePath = ""; // adicionar o caminho aqui
+
+  const absolutePath = path.resolve(filePath);
+
+  fs.stat(absolutePath, (err, stats) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        return res.status(404).json({ error: "Arquivo não encontrado." });
+      }
+      return res.status(500).json({ error: "Erro ao acessar o arquivo." });
+    }
+
+    res.json({ tamanho: stats.size, unidade: "bytes" });
+  });
+});
 
 app.listen(3333, () => {
   console.log("listening on port 3333");
